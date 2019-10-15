e3bbf58f0f19cb209026a278f86ab1a8b453ee90
Hina Shamim <hs434@njitdm.campus.njit.edu>
2019-10-15 19:12:51 -0400

    Fix: update the print message for test in ExperimentTests_hw_2.py

--- src/ExperimentTests_hw_2.py
+++ src/ExperimentTests_hw_2.py
@@ -7,7 +7,7 @@ from Experiment_hw_1 import Greeter
 class MyTestCase(unittest.TestCase):
     def test_default_greeting_set(self):
         greeter = Greeter()
-        self.assertEqual(greeter.message, 'Hello world!')
+        self.assertEqual(greeter.message, 'Welcome to this interesting world!')
 
 
 if __name__ == '__main__':
